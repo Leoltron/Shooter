775fe0240c6250ac004aa5bf2fe77b1ae3134894
Leoltron <leoole5@gmail.com>
2017-05-13 16:51:25 +0500

    Added testing project

---
+++ Shooter.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Shooter.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Shooter.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("40a5d41e-db16-4b82-a0af-ba0ecfb164f4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Shooter.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Shooter.Tests
+{
+    [TestFixture]
+    public class EntityTests
+    {
+        //TODO: Make tests
+    }
+}
